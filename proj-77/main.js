acea95cb1512480548656a89057c5730fe03699d
Fact-shreyas <85051434+Fact-shreyas@users.noreply.github.com>
2021-12-10 23:37:45 +0530

    Add files via upload

---
+++ proj-77/main.js
@@ -0,0 +1,9 @@
+var con="i am Shreyas and I developed this "
+function nextbolo(){
+console.log(con)
+console.log(gjg)
+console.log("console :)")
+console.log("thanks and bye ")
+}
+var gjg="but i also told you the shortcut to open: "
+
